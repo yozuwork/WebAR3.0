bddc9b058545564fb6acda2c59340cd4ea7597a8
yozu <yozuwork@gmail.com>
2024-07-19 13:53:43 +0800

    chore: Add .DS_Store to .gitignore files

---
+++ WebAR-Demo-main/WEBAR-Demo-ImageTracking/assets/events.js
@@ -0,0 +1,26 @@
+var step = 1;
+var is_notified = false;
+AFRAME.registerComponent('image-tracker-1', {
+    init: function () {
+        console.log("image-tracker-1 init");
+    },
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            // 掃描到辨識點
+            step = 2;
+        }
+    }
+});
+AFRAME.registerComponent('image-tracker-2', {
+    init: function () {
+        console.log("image-tracker-2 init");
+    },
+    tick: function() {
+        if (this.el.object3D.visible == true) {
+            if (step != 2 && !is_notified) {
+                is_notified = true;
+                alert("請先掃描封面辨識點");
+            }
+        }
+    }
+});
